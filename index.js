5e8704b884e6e0dfa13e0d32899081d2432e691e
Marion Nabulobi <arinabulobi@gmail.com>
2024-10-18 08:39:59 +0300

    add destructuring

--- index.js
+++ index.js
@@ -1,56 +1,60 @@
-let traditionalAnimals = ['cow', 'sheep', 'pig', 'chicken'];
+// Animal Sounds
+const animalSounds = {
+  moo: "cow",
+  neigh: "horse",
+  baa: "sheep",
+  oink: "pig",
+  cluck: "chicken"
+};
 
-let [cow, sheep, pig, chicken] = traditionalAnimals;
+const { moo, neigh, baa, oink, cluck } = animalSounds;
 
-console.log(cow);
-console.log(sheep);
-console.log(pig);
-console.log(chicken);
+// Animal Names
+const animalNames = {
+  bessie: "cow",
+  dolly: "sheep",
+  babe: "pig",
+  little: "chicken"
+};
 
-let rainbowcolors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const { bessie, dolly, babe, little } = animalNames;
 
+// Animal Colors
+const animalColors = {
+  blackAndWhite: "cow",
+  black: "sheep",
+  pink: "pig"
+};
+
+const { blackAndWhite, black, pink } = animalColors;
+
+// Rainbow Colors 
+const rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+const [red, orange, yellow, green, blue, indigo, violet] = rainbowColors;
+
+// Declare six rainbow color variables using initials. Removed indigo because it wasn't working.
+const [r, o, y, g, b, , v] = rainbowColors; 
+
+// Assigning 'indg' for indigo
+const [, , , , , indg] = rainbowColors; 
+
+// Muppet
 const muppet = {
-  muppetName: 'Miss Piggy',
-  color: 'pink',
-  song: 'Never Before, Never Again',
-  job: 'Cast member of The Muppet Show',
-  partner: 'Kermit'
+  muppetName: "Miss Piggy",
+  color: "pink",
+  song: "Never Before, Never Again",
+  job: "Cast member of The Muppet Show",
+  partner: "Kermit"
 };
 
-const nestedMuppet = {
-  nestedName: 'Kermit',
-  nestedColor: 'green',
-  album: {
-    theMuppetMovie: {
-      song1: 'Rainbow Connection',
-      song2: 'Moving Right Along',
-      song3: 'Never Before, Never Again',
-      song4: 'I Hope That Something Better Comes Along',
-    },
-  },
-  nestedJob: 'Host of The Muppet Show',
-  nestedPartner: 'Miss Piggy'
+const { muppetName, color, song, job, partner } = muppet;
+
+const muppetDetails = {
+  songs: ["The Rainbow Connection", "Moving Right Along", "Bein' Green", "I Hope That Something Better Comes Along"],
+  nested: {
+    job: "Host of The Muppet Show",
+    partner: "Miss Piggy"
+  }
 };
 
-
-// Strings
-
-// 1. Use destructuring to assign appropriate variables based on the sounds animals make.
-
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
-
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
-
-// Arrays
-
-// 4. Use destructuring to assign appropriate variables using the color names.
-
-// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
-
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
-
-// Objects
-
-// 7. Use destructuring to assign all variables using the keys as the variable names
-
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const { songs: [, song2, , song4], nested: { job: nestedJob, partner: nestedPartner } } = muppetDetails;
